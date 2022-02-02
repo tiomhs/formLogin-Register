489ba1178979dc27fbd5d51984d578bce3afb094
tiomhs <88199655+tiomhs@users.noreply.github.com>
2022-02-02 22:40:58 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+
+<h1>Login Page</h1>
+
+<form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username : </label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password : </label>
+                <input type="password" name="password" id="password">
+            </li>
+            <button type="submit" name="submit">Login!</button>
+            <p>Don't have an Account?<a href="register.php">click here!!!</a></p>
+        </ul>
+</form>
+    
+</body>
+</html>
